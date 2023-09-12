adcf24ad0969ba7ccd227d70c4ad3259b3676f75
nomvula <nzshabangu95@gmail.com>
2023-09-12 11:51:00 +0200

    Add project files.

---
+++ 11. if statements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the test that can be true ir false
+            bool isMale = true;
+            if(isMale)
+            {
+                Console.WriteLine("you are male");
+            }
+            else
+            {
+                Console.WriteLine("you are female");
+            }
+            //can also test 2 conditions with and 
+            bool isTall = true;
+            if(isTall && isMale) {
+
+                Console.WriteLine("you are both male and tall");
+            }
+
+            else if (isTall && isMale)
+            {
+                Console.WriteLine("you are not tall but male ");
+            }
+            else if (isTall && isMale)
+            {
+                Console.WriteLine("you are neither a man nor tall");
+            }
+            else
+                    {
+                        
+                Console.WriteLine("you are none");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
